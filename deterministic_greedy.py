51fda9e0925e32f0aac890aeda912aef5356a87f
Arik Pamnani <arikpamnani@gmail.com>
2017-04-15 20:17:56 +0530

    added

--- deterministic_greedy.py
+++ deterministic_greedy.py
@@ -4,14 +4,11 @@ conf = SparkConf().setMaster("local").setAppName("test")
 sc = SparkContext(conf = conf)
 
 k = 100	# number of partitions
+part = []
+for i in range(k):
+	part.append([])
 file_name = "/home/arik/graph-partitioning/p2p-Gnutella04.txt"
 lines = sc.textFile(file_name, k)
-
-def graph_partition(v, k):
-	# vertex v
-	# ind -> {0, 1, 2, . . . , k-1}
-	ind = v%k
-	return ind
 
 def conv(x):
 	x = x.split()
@@ -26,6 +23,13 @@ max_node = edges.max(lambda x: max(x[0], x[1]))
 
 # create an Adjacency list of the graph
 AL = edges.reduceByKey(lambda a, b: a+b)
-wp = AL.partitionBy(k, lambda x: graph_partition(x, k))
-print wp.glom().collect()
+# AL.persist()	# persist the AL RDD
+AL_list = AL.glom().collect()
 
+d = {i:-1 for i in range(max_node[0]+1)}
+def graph_partition(v, k):
+	for i in range(len(AL_list)):
+		print AL_list[i][0]
+graph_partition(2, k)
+
+# wp = AL.partitionBy(k, lambda x: graph_partition(x, k))
